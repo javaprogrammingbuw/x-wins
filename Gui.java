a149a6f70f1151bb5ce70ecbf8757b81d280b43b
NadeemAhmad007 <44241167+NadeemAhmad007@users.noreply.github.com>
2019-04-27 09:44:40 +0200

    Update Gui.java

--- Gui.java
+++ Gui.java
@@ -1,7 +1,11 @@
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import javax.swing.border.*;
+
+
+
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
 
@@ -14,9 +18,16 @@ public class Gui extends JFrame{
 	private JTextField tf2;
 	private JTextField tf3;
 	private JButton calcBtn;
-	private JButton[][] squares = new JButton[8][8];
-	private boolean turn = false;
-
+	private int rowInt;
+	private int colInt;
+	private int stnInt;
+	private String row ;
+	private String col ;
+	private String stn;
+	private JButton[][] squares;
+	private boolean [][] state;
+	private boolean turn;
+	
 	public Gui(){
 		initUi();
 	}
@@ -26,20 +37,39 @@ public class Gui extends JFrame{
 
 		inputPane = new JPanel();
 		tf1 = new JTextField();
-		tf1.setText("Rows");
+		tf1.setText("0");
+		
 		tf2 = new JTextField();
-		tf2.setText("Columns");
+		tf2.setText("0");
+		
 		tf3 = new JTextField();
-		tf3.setText("Stones to win");
+		tf3.setText("0");
+		
 		calcBtn = new JButton("Compute");
+		
+		
+		
+		
 		//Todo: Thing about how to adjust the parameters
-		calcBtn.addActionListener((event) -> initField(2,2,2));
+		calcBtn.addActionListener((event) ->{
+			row = tf1.getText();
+			rowInt=Integer.parseInt(row);
+			col = tf2.getText();
+			colInt=Integer.parseInt(col);
+			stn=tf3.getText();
+			stnInt=Integer.parseInt(stn);
+			squares=new JButton[rowInt][colInt];
+			state= new boolean[rowInt][colInt];
+		   initField(rowInt,colInt,stnInt);
+		   
+		});
+		
 		inputPane.add(tf1);
 		inputPane.add(tf2);
 		inputPane.add(tf3);
 		inputPane.add(calcBtn);
 		inputPane.setLayout(new GridLayout(1,4));
-
+		
 		
 
 		contentPane.add(inputPane);
@@ -53,49 +83,271 @@ public class Gui extends JFrame{
 	}
 
 	private void initField(int width, int height, int stones){
-		outputPane= new JPanel(new GridLayout(8, 8));
-		outputPane.setPreferredSize(new Dimension(1200, 1200));
-        outputPane.setBorder(new LineBorder(Color.BLACK));
+		
+		
+		
+		outputPane= new JPanel(new GridLayout(width, height));
+		outputPane.setPreferredSize(new Dimension(1200*stones/width,1200*stones/height));
+		outputPane.setBorder(new LineBorder(Color.BLACK));
 
 		for (int i = 0; i < squares.length; i++) {
-            for (int j = 0; j < squares[i].length; j++) {
+                 for (int j = 0; j < squares[i].length; j++) {
                 JButton b = new JButton();
                 ImageIcon icon = new ImageIcon(
-                    	new BufferedImage(64, 64, BufferedImage.TYPE_INT_ARGB));
+                new BufferedImage(64, 64, BufferedImage.TYPE_INT_ARGB));
+               
+                 final int x = j;
+                 final int y = i;
+                 
+                 squares[j][i]=b;
+                 state [j][i]=false;   
+
+                b.addActionListener((event) -> {
+    			 addStone(b,x,y);
+             	
+           	});
+             
                 b.setIcon(icon);
-                final int x = j;
-                final int y = i;
                 b.setBackground(Color.BLUE);
-                b.addActionListener((event) -> addStone(b));
-                squares[j][i] = b;
                 outputPane.add(squares[j][i]);
-            }
-        }
+                  }
+                }
+  
+      
         contentPane.add(outputPane);
         contentPane.setLayout(new BoxLayout(contentPane, BoxLayout.Y_AXIS));
+       
         pack();
 	}
 
-	private void addStone(JButton b){
-		BufferedImage circleImg = new BufferedImage(120, 120, BufferedImage.TYPE_INT_ARGB);
+    
+	
+
+	
+	private void addStone(JButton  b,int x,int y){
+		BufferedImage circleImg = new BufferedImage(100,100, BufferedImage.TYPE_INT_ARGB);
       	Graphics2D g2 = circleImg.createGraphics();
       	g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-      	if(!turn){
+    
+      
+   		 for ( int r=y;r<=squares.length-1;r++) {
+ 		
+   			 if(state[x][r]==false) {
+	 	 	 b=squares[x][r];
+	 	 	 y = r;
+   			}
+   		}
+   	 
+    
+   	 
+	  	if(!turn){
+		
       		g2.setColor(Color.RED);
+      		state[x][y] = true;
       		turn = true;
-      	}
-      	else{
+      	
+      	  
+      		}
+      		else{
+      		
       		g2.setColor(Color.YELLOW);
+      		state[x][y] = true;
       		turn = false;
-      	}
-      	g2.fillOval(4,4,115,115);
+      		
+      	
+   	 	}
+		
+      	g2.fillOval(4,4,95,95);
       	g2.dispose();
       	b.setIcon(new ImageIcon(circleImg));
+   	    b.setVisible(true);
+   	    getWinner(x,y);
 	}
+	
+	void getWinner(int x,int y) { 	
+
+		int s1 = 0, s2 = 0; //counter for player 1 und 2
+		
+		// horizontal check
+		for (int i = y; i <rowInt ; i++) { // y direction
+			for (int j = x; j < colInt; j++) {// x direction
+				if (state[i][j]==true && turn== true){
+					s1+=1;
+					for (int h = 1; h<stnInt; h++){
+						if (i+h<rowInt && state[i+h][j]==true && turn == true ) {
+							s1+=1;
+							
+						}
+						else {
+							s1=0;
+						}
+					}
+				}
+				else {
+					s1=0;
+				}
+				
+				if(s1==stnInt) {
+					JOptionPane.showMessageDialog (null, "red won!");
+					
+				}
+				
+		if (state[i][j]==true && turn == false ){
+					s2+=1;
+					for (int h = 1; h<stnInt; h++){
+						if (i+h<rowInt && state [i+h][j]==true && turn == false ) {
+							s2+=1;
+						}
+						else {
+							s2=0;
+						}
+					}
+				}
+				else {
+					s2=0;
+				}
+				if(s2==stnInt) {
+					JOptionPane.showMessageDialog (null, "yellow won!");
+					
+				}
+			}	
+		}
+		// vertical check
+		for (int i = y; i < rowInt; i++) { // y direction
+			for (int j = x; j < colInt; j++) {// x direction
+				if (state[i][j]==true && turn == true ){
+					s1+=1;
+					for (int h = 1; h<stnInt; h++){
+						if (j+h<colInt && state[i][j+h]==true && turn == true ) {
+							s1+=1;
+						}
+						else {
+							s1=0;
+						}
+					}
+				}
+				else {
+					s1=0;
+				}
+				if(s1==stnInt) {
+					JOptionPane.showMessageDialog (null, "red won!");
+				}
+				
+				
+				if (state[i][j]==true && turn == false  ){
+					s2+=1;
+					for (int h = 1; h<stnInt; h++){
+						if (j+h<colInt && state[i][j+h]==true && turn == false) {
+							s2+=1;
+						}
+						else {
+							s2=0;
+						}
+					}
+				}
+				else {
+					s2=0;
+				}
+				if(s2==stnInt) {
+					JOptionPane.showMessageDialog (null, "yellow won!");
+				}
+				
+			}	
+		}
+		
+		// diagonal check left top to right bottom
+		for (int i = y; i < rowInt; i++) { // y direction
+			for (int j = x; j < colInt; j++) {//// x direction
+				if (state[i][j]==true && turn == true ){
+					s1+=1;
+					for (int h = 1; h<stnInt; h++){
+						if (j+h<colInt && i+h<rowInt && state [i+h][j+h]==true && turn ==true  ) {
+							s1+=1;
+						}
+						else {
+							s1=0;
+						}
+					}
+				}
+				else {
+					s1=0;
+				}
+				if(s1==stnInt) {
+					JOptionPane.showMessageDialog (null, "red won!");
+				}
+			
+				
+				if (state[i][j]==true && turn == false ){
+					s2+=1;
+					for (int h = 1; h<stnInt; h++){
+						if (j+h<colInt &&i+h<rowInt&& state [i+h][j+h]==true && turn == false ) {
+							s2+=1;
+						}
+						else {
+							s2=0;
+						}
+					}
+				}
+				else {
+					s2=0;
+				}
+				if(s2==stnInt) {
+					JOptionPane.showMessageDialog (null, "yellow won!");
+				}
+			
+			}	
+		}
+		
+		// diagonal check right top to left bottom
+		for (int i = 0; i < rowInt; i++) { // y direction
+			for (int j = 0; j < colInt; j++) {// x direction
+				if (state[i][j]==true && turn == true){
+					s1+=1;
+					for (int h = 1; h<stnInt; h++){
+						if (j-h>0 && i+h<7 && state[i+h][j-h]==true && turn ==true) {
+							s1+=1;
+						}
+						else {
+							s1=0;
+						}
+					}
+				}
+				else {
+					s1=0;
+				}
+				if(s1==stnInt) {
+					JOptionPane.showMessageDialog (null, "red won!");
+				}
+			
+				
+				if (state[i][j]==true && turn == false ){
+					s2+=1;
+					for (int h = 1; h<stnInt; h++){
+						if (j-h>0 &&i+h<7 && state [i+h][j-h]==true && turn == false ) {
+							s2+=1;
+						}
+						else {
+							s2=0;
+						}
+					}
+				}
+				else {
+					s2=0;
+				}
+				if(s2==stnInt) {
+					JOptionPane.showMessageDialog (null, "yellow won!");
+				}
+				
+			}	
+		}
+		
+	}
+
 
 	public static void main(String[] args){
 		EventQueue.invokeLater(() ->{
             Gui gui = new Gui();
+            
             gui.setVisible(true);
         });
 	}
